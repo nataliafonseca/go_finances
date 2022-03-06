6779883feb2889c349299c1b4ac887e57a39eed7
Natália Fonseca <nataliabf@outlook.com>
2022-03-06 18:22:44 -0300

    feat: add header

--- src/screens/Dashboard/index.tsx
+++ src/screens/Dashboard/index.tsx
@@ -1,10 +1,24 @@
 import React from "react";
+
 import * as S from "./styles";
 
 export function Dashboard() {
   return (
     <S.Container>
-      <S.Title>Dashboard</S.Title>
+      <S.Header>
+        <S.UserWrapper>
+          <S.UserInfo>
+            <S.Avatar
+              source={{ uri: "https://github.com/nataliafonseca.png" }}
+            />
+            <S.User>
+              <S.UserGreeting>Olá,</S.UserGreeting>
+              <S.UserName>Natália</S.UserName>
+            </S.User>
+          </S.UserInfo>
+          <S.Icon name="power" />
+        </S.UserWrapper>
+      </S.Header>
     </S.Container>
   );
 }
--- src/screens/Dashboard/styles.ts
+++ src/screens/Dashboard/styles.ts
@@ -1,14 +1,56 @@
+import { Feather } from "@expo/vector-icons";
+import { RFPercentage, RFValue } from "react-native-responsive-fontsize";
 import styled from "styled-components/native";
 
 export const Container = styled.View`
   flex: 1;
-  justify-content: center;
-  align-items: center;
   background-color: ${({ theme }) => theme.colors.background};
 `;
 
-export const Title = styled.Text`
+export const Header = styled.View`
+  width: 100%;
+  height: ${RFPercentage(42)}px;
+  background-color: ${({ theme }) => theme.colors.primary};
+  justify-content: center;
+  align-items: center;
+`;
+
+export const UserWrapper = styled.View`
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+  width: 100%;
+  padding: 0 ${RFValue(24)}px;
+`;
+
+export const UserInfo = styled.View`
+  flex-direction: row;
+  align-items: center;
+`;
+
+export const Avatar = styled.Image`
+  height: ${RFValue(48)}px;
+  width: ${RFValue(48)}px;
+  border-radius: 10px;
+`;
+
+export const User = styled.View`
+  margin-left: ${RFValue(17)}px;
+`;
+
+export const UserGreeting = styled.Text`
+  color: ${({ theme }) => theme.colors.shape};
+  font-family: ${({ theme }) => theme.fonts.regular};
+  font-size: ${RFValue(18)}px;
+`;
+
+export const UserName = styled.Text`
+  color: ${({ theme }) => theme.colors.shape};
   font-family: ${({ theme }) => theme.fonts.bold};
-  font-size: 24px;
-  color: ${({ theme }) => theme.colors.title};
+  font-size: ${RFValue(18)}px;
+`;
+
+export const Icon = styled(Feather)`
+  color: ${({ theme }) => theme.colors.secondary};
+  font-size: ${RFValue(24)}px;
 `;
